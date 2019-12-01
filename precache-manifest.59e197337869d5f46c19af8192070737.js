8fd43fb0bafa0953dd2184f4da07156520b8b89b
gleb <gkebkasyak@gmail.com>
2019-12-02 00:15:44 +0300

    Updates

---
+++ precache-manifest.59e197337869d5f46c19af8192070737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a20bbfd9be318307bfb9e65c8df6d1",
+    "url": "/Hacker-News/index.html"
+  },
+  {
+    "revision": "8575da371c04c1063a32",
+    "url": "/Hacker-News/static/css/main.f13fd9cf.chunk.css"
+  },
+  {
+    "revision": "7233959a3cd27037728a",
+    "url": "/Hacker-News/static/js/2.fc425cda.chunk.js"
+  },
+  {
+    "revision": "8575da371c04c1063a32",
+    "url": "/Hacker-News/static/js/main.6bc2bdd3.chunk.js"
+  },
+  {
+    "revision": "364adfddeea8889f411c",
+    "url": "/Hacker-News/static/js/runtime-main.0264ec01.js"
+  }
+]);
